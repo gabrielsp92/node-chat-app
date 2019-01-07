6a7249682dcddcbc85e4b2020e38825c0a64e9b0
Gabriel Soares <gsoaresp92@gmail.com>
2019-01-07 12:21:25 -0200

    Init commit

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const express = require('express');
+
+
+const publicPath = path.join(__dirname,'../public');
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.static(publicPath));
+
+app.get('/',(req, res) => {
+
+});
+
+app.listen(port,()=>{
+    console.log(`server is up on port ${port}`);
+})
